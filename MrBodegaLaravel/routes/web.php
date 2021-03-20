b8601d220007c6d567bd7b0a4fea18528acd69fb
fernandomanuel123 <foi.123@hotmail.com>
2021-03-20 17:02:57 -0500

    update welcome page

--- MrBodegaLaravel/routes/web.php
+++ MrBodegaLaravel/routes/web.php
@@ -13,14 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
-});
+});*/
+Route::get('/', [App\Http\Controllers\WelcomeController::class, 'index'])->name('welcome');
 
-Auth::routes();
+Route::get('/home', [App\Http\Controllers\WelcomeController::class, 'index'])->name('home');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
