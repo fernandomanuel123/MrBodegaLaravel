07a2618b9088e5e629f54bebee080ec1ec08ca00
Pablo Galarza <pgalarza@itana.pe>
2021-04-24 10:32:00 -0500

    Se agrego ids a componentes html

--- MrBodegaLaravel/app/Http/Controllers/AdmUsuarioController.php
+++ MrBodegaLaravel/app/Http/Controllers/AdmUsuarioController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-
+use App\Form;  
 class AdmUsuarioController extends Controller
 {
     /**
@@ -14,20 +14,27 @@ class AdmUsuarioController extends Controller
      */
     public function __construct()
     {
-       
     }
 
-    
-    public function acceder(Request $request) {
+    public function importfile(Request $request)
+    {
+            if($request->hasFile('upload_file')){
+                $logoImage = $request->file('upload_file');
+                $name = $logoImage->getClientOriginalName();
+                $data = Http::post('http://localhost:5000/api/FileUpload', [
+                    
+                ]);
+                return $name;
+            } else {
+                return "Error al subir archivo";
+            }
+        
+    }
 
-    if ($request->has('fileUpload')) {
-        $fileUpload = $request->input('fileUpload');
-        dd($fileUpload);
+    public function acceder(Request $request)
+    {
 
-        $usuarios = HTTP::get('http://localhost:5000/api/Usuario');
-        $ArrayUsuarios = $usuarios->json();
-        return view('adm-usuario')->with('ArrayUsuarios', $ArrayUsuarios);
-    } else {
+
         $token = null;
         $email = $request->input('email');
         $password = $request->input('password');
@@ -40,7 +47,7 @@ class AdmUsuarioController extends Controller
 
         //Storage::disk('local')->get('token');
         //Storage::disk('local')->put('token', $data['token']);
-  
+
         if ($data['token']) {
             $usuarios = HTTP::get('http://localhost:5000/api/Usuario');
             $ArrayUsuarios = $usuarios->json();
@@ -49,21 +56,16 @@ class AdmUsuarioController extends Controller
             return "Credenciales incorrectas";
         }
     }
-       
-        
-    }
-    
+
     public function index()
     {
         $usuarios = HTTP::get('http://localhost:5000/api/Usuario');
         $ArrayUsuarios = $usuarios->json();
         return view('adm-usuario')->with('ArrayUsuarios', $ArrayUsuarios);
-       
     }
 
-    public function exportPDF() {
+    public function exportPDF()
+    {
         return 'a';
     }
-
-
 }
--- MrBodegaLaravel/routes/web.php
+++ MrBodegaLaravel/routes/web.php
@@ -37,7 +37,7 @@ Route::put('/productos', [App\Http\Controllers\ProductosController::class, 'edit
 Route::get('/boletas', [App\Http\Controllers\BoletasController::class, 'index'])->name('boletas');
 Route::post('/boletas', [App\Http\Controllers\BoletasController::class, 'enviarboletas'])->name('boletas');
 
-
+Route::post('/importfile', [App\Http\Controllers\AdmUsuarioController::class, 'importfile'])->name('importfile');
 
 Route::post('/save-usuario', [App\Http\Controllers\SaveUsuarioController::class, 'saveUsuarioView'])->name('save-usuario');
 Route::post('/guardar-usuario', [App\Http\Controllers\SaveUsuarioController::class, 'guardarUsuario'])->name('guardar-usuario');
